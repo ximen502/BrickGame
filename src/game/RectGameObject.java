fcfc001d48f589f54ea67d27979e8c5264437520
ximen502 <ximen502@126.com>
2020-04-16 23:04:03 +0800

    修改方法的变量命名

--- src/game/RectGameObject.java
+++ src/game/RectGameObject.java
@@ -12,23 +12,27 @@ public abstract class RectGameObject extends GameObject {
     /**
      * 判断矩形是不是在一个象限里面(参考CSDN某大牛的碰撞检测JS代码，作者牛批)
      *
-     * @param cood 圆形小球的圆心坐标
+     * @param center 圆形小球的圆心坐标
      * @param objA 矩形的左上角坐标
      * @param objB 矩形的右下角坐标
      * @return 圆形小球和矩形是否处于同一个直角坐标系象限
      */
-    protected boolean isSameQuadrant(Point cood, Point objA, Point objB) {
-        int coodX = cood.x;
-        int coodY = cood.y;
+    protected boolean isSameQuadrant(Point center, Point objA, Point objB) {
+        int cX = center.x;
+        int cY = center.y;
         int xoA = objA.x, yoA = objA.y, xoB = objB.x, yoB = objB.y;
 
-        if (xoA - coodX > 0 && xoB - coodX > 0) {
-            if ((yoA - coodY > 0 && yoB - coodY > 0) || (yoA - coodY < 0 && yoB - coodY < 0)) {
+        int deltaACX = xoA - cX;
+        int deltaBCX = xoB - cX;
+        int deltaACY = yoA - cY;
+        int deltaBCY = yoB - cY;
+        if (deltaACX > 0 && deltaBCX > 0) {
+            if ((deltaACY > 0 && deltaBCY > 0) || (deltaACY < 0 && deltaBCY < 0)) {
                 return true;
             }
             return false;
-        } else if (xoA - coodX < 0 && xoB - coodX < 0) {
-            if ((yoA - coodY > 0 && yoB - coodY > 0) || (yoA - coodY < 0 && yoB - coodY < 0)) {
+        } else if (deltaACX < 0 && deltaBCX < 0) {
+            if ((deltaACY > 0 && deltaBCY > 0) || (deltaACY < 0 && deltaBCY < 0)) {
                 return true;
             }
             return false;
